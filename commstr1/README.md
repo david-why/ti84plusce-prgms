200c750f917a70080b96a564872902c052135bd7
David <david_why@outlook.com>
2021-08-16 16:13:44 +0800

    feat: Add project commstr1

---
+++ commstr1/README.md
@@ -0,0 +1,13 @@
+# commstr1
+This program contains two subprograms: `recvstr1` and `sendstr1`.
+They are used to send the `Str1` variable from one calculator to another.
+
+Before running either program, connect the two calculators with an USB cable.
+
+## sendstr1
+Run this program on the sender.
+When the program shows "Write finished, please disconnect", disconnect the USB cable and press <kbd>On</kbd>.
+
+## recvstr1
+Run this program on the receiver.
+When the sender shows "Write finished, please disconnect", disconnect the USB cable and press <kbd>On</kbd>.
---
+++ commstr1/sendstr1/src/main.cpp
@@ -0,0 +1,116 @@
+#include <srldrvce.h>
+#include <usbdrvce.h>
+#include <tice.h>
+#include <fileioc.h>
+#include <keypadc.h>
+
+#include <stdio.h>
+
+usb_device_t device;
+srl_device_t srl;
+bool has_srl_device;
+uint8_t srl_buf[512];
+char strbuf[128];
+
+static usb_error_t handle_usb_event(usb_event_t event, void *event_data,
+                                    usb_callback_data_t *callback_data __attribute__((unused)))
+{
+    sprintf(strbuf, "<%u>", event);
+    os_PutStrFull(strbuf);
+    /* Enable newly connected devices */
+    if (event == USB_DEVICE_CONNECTED_EVENT && !(usb_GetRole() & USB_ROLE_DEVICE))
+    {
+        usb_device_t device = (usb_device_t)event_data;
+        os_PutStrFull("connected");
+        usb_ResetDevice(device);
+    }
+    if (event == USB_HOST_CONFIGURE_EVENT)
+    {
+        usb_device_t host = usb_FindDevice(NULL, NULL, USB_SKIP_HUBS);
+        os_PutStrFull("configure");
+        if (host)
+            device = host;
+    }
+    /* When a device is connected, or when connected to a computer */
+    if ((event == USB_DEVICE_ENABLED_EVENT && !(usb_GetRole() & USB_ROLE_DEVICE)))
+    {
+        os_PutStrFull("enable");
+        device = (usb_device_t)event_data;
+    }
+    if (event == USB_DEVICE_DISCONNECTED_EVENT && device)
+    {
+        os_PutStrFull("disconnect");
+        srl_Close(&srl);
+        has_srl_device = false;
+        device = NULL;
+    }
+
+    return USB_SUCCESS;
+}
+
+int main()
+{
+    os_ClrHome();
+    kb_EnableOnLatch();
+
+    ti_CloseAll();
+    ti_var_t slot = ti_OpenVar(ti_Str1, "r", TI_STRING_TYPE);
+    if (!slot)
+        return 1;
+
+    const usb_standard_descriptors_t *desc = srl_GetCDCStandardDescriptors();
+    if (usb_Init(handle_usb_event, NULL, desc, USB_DEFAULT_INIT_FLAGS))
+    {
+        usb_Cleanup();
+        os_PutStrFull("USB init error");
+        delay(2000);
+        return 1;
+    }
+
+    bool connected = false;
+    os_PutStrFull("Waiting for USB connection");
+    do
+    {
+        usb_HandleEvents();
+
+        if (device && !has_srl_device)
+        {
+            if (srl_Open(&srl, device, srl_buf, sizeof(srl_buf), SRL_INTERFACE_ANY, 115200))
+            {
+                usb_Cleanup();
+                os_PutStrFull("Serial open error");
+                delay(2000);
+                return 1;
+            }
+
+            has_srl_device = true;
+            connected = true;
+
+            os_PutStrFull("USB connected, press Enter to send");
+            do
+                kb_ScanGroup(6);
+            while (!kb_IsDown(kb_KeyEnter));
+            do
+                kb_ScanGroup(6);
+            while (kb_IsDown(kb_KeyEnter));
+
+            os_ClrHome();
+            uint8_t bytes_read;
+            size_t total_read = 0;
+            while ((bytes_read = ti_Read(strbuf, 1, 128, slot)))
+            {
+                total_read += bytes_read;
+                srl_Write(&srl, strbuf, bytes_read);
+                sprintf(strbuf, "%u", total_read);
+                os_HomeUp();
+                os_PutStrFull(strbuf);
+            }
+
+            os_ClrHome();
+            os_PutStrFull("Write finished, please    disconnect");
+        }
+    } while ((!connected || has_srl_device) && !kb_On);
+
+    usb_Cleanup();
+    return 0;
+}
