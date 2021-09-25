792db6ce665ee03ef31ec9780fccaa9282aa2185
David <david_why@outlook.com>
2021-09-25 21:58:03 +0800

    fix: Remove quotes around name

--- mkproj.sh
+++ mkproj.sh
@@ -7,5 +7,5 @@ fi
 if [ -e "$1" ]; then echo 'error: file/directory exists'; exit 1; fi
 cp -r template "$1"
 sed -i 's/# \[name\]/# '"$1"'/g' "$1"/README.md
-sed -i 's/= name/= "'"${1^^}"'"/g' "$1"/makefile
+sed -i 's/= name/= '"${1^^}"'/g' "$1"/makefile
 
