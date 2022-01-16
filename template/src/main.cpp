fd4cb422b9ec8ea201ee54c34514dfeb4da4a346
David <david_why@outlook.com>
2022-01-16 20:51:55 +0800

    fix: Use __INT24_TYPE__ instead of HAS_PRINTF

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,6 +1,11 @@
-#ifndef HAS_PRINTF
-typedef unsigned int uint24_t;
-typedef int int24_t;
+#ifndef __INT24_TYPE__
+#define __INT24_TYPE__ int
+#define __INT24_MAX__ 0x7fffff
+#define __INT24_MIN__ (~__INT24_MAX__)
+typedef __INT24_TYPE__ int24_t;
+#define __UINT24_TYPE__ unsigned int
+#define __UINT24_MAX__ 0xffffff
+typedef __UINT24_TYPE__ uint24_t;
 #endif
 
 #include <tice.h>
